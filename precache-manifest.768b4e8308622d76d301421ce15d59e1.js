3ff99e3db4e8e7fb442486b628927a945f2a6d42
Tigran-dev-1992 <tikokarap@mail.ru>
2021-02-24 13:08:12 +0400

    Updates

---
+++ precache-manifest.768b4e8308622d76d301421ce15d59e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cc0cb019831143a592a8ee0bcadaf3",
+    "url": "/firstproject/index.html"
+  },
+  {
+    "revision": "0abf8d7953525e0c333d",
+    "url": "/firstproject/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "d170450b8832f0888c48",
+    "url": "/firstproject/static/css/4.545a1ba7.chunk.css"
+  },
+  {
+    "revision": "e58e77f83f16e078f239",
+    "url": "/firstproject/static/css/main.21d27f40.chunk.css"
+  },
+  {
+    "revision": "0abf8d7953525e0c333d",
+    "url": "/firstproject/static/js/2.db5fb7c8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/firstproject/static/js/2.db5fb7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763de5f7fb98d8a50ec9",
+    "url": "/firstproject/static/js/3.3d1e0594.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/firstproject/static/js/3.3d1e0594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d170450b8832f0888c48",
+    "url": "/firstproject/static/js/4.433c5a03.chunk.js"
+  },
+  {
+    "revision": "69bfd34b57f896706b83",
+    "url": "/firstproject/static/js/5.bbdbc20b.chunk.js"
+  },
+  {
+    "revision": "e58e77f83f16e078f239",
+    "url": "/firstproject/static/js/main.3156abef.chunk.js"
+  },
+  {
+    "revision": "9db41eb5f6cbfc5e2d87",
+    "url": "/firstproject/static/js/runtime-main.0f068d78.js"
+  },
+  {
+    "revision": "cd9a1ab441debdb4e14991c7894c3cd2",
+    "url": "/firstproject/static/media/logo.cd9a1ab4.png"
+  },
+  {
+    "revision": "2b02159fee58d573c079ad5212d56b63",
+    "url": "/firstproject/static/media/preLoader.2b02159f.gif"
+  },
+  {
+    "revision": "e1529f18c5dd33491837b6914e1d3604",
+    "url": "/firstproject/static/media/userPhoto.e1529f18.jpg"
+  }
+]);
